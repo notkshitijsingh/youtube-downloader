60467fb7fedda82b6b4f12d77179ca98d7a2df1c
Kshitij Singh <66397721+notkshitijsingh@users.noreply.github.com>
2024-09-11 15:06:11 +0530

    Update for Deployment

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def serve_file(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
